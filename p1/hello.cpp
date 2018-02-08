9b52e6b7d71e4797d4243bf0567d1bff3dc41a15
Bujar Sefa <bsefa000@citymail.ccny.cuny.edu>
2018-02-08 12:49:07 -0500

    Updated grammar in hello.cpp file

--- p1/hello.cpp
+++ p1/hello.cpp
@@ -7,32 +7,27 @@
  * NOT mean it is okay to COPY THAT SOURCE.  What you submit here **MUST BE
  * YOUR OWN WORK**.
  * References:
- *
+ * Project webpage to get all of the code above int main
  */
-
-
-/* NOTE resources used: 
- * Project webpage to get all the code above main.
- * */
 
 #include <iostream> //Adds iostream library
 using std::cin; //Helps us reduce use of std::cin
 using std::cout; //Helps us reduce use of std::cout
 #include <string> //Adds string library
-using std::string; //not sure what this actually does  2/5/18 //Update: I believe its to allow is to use the string variable, still unsure.
+using std::string; //not sure what this actually does  2/5/18 //Update: I believe it is to allow it to use the string variable, still unsure.
 int main(){ //Where the code goes inside
 	string yourName; //String varible where name will be stored.
 	string relative; //String variable where relative will be stored.
 	cout << "Welcome to my first project. Please enter your name: ";
 	//cin >> name; //note this is what I originally planned to use.
-	getline(cin, yourName); 
-	/* NOTE what ^this code does is it says 
-	 * take the inputted line from the compiler 
-	 * using the function cin, 
+	getline(cin, yourName);
+	/* NOTE what ^this code does is it says
+	 * take the inputted line from the compiler
+	 * using the function cin,
 	 * and then store that into name */
 	cout << "Enter the name of your relative: ";
 	getline(cin, relative); //Gets relative's name and stores it.
-	cout << "Hello. My name is " << yourName << ". You killed my " << relative << ". Prepare to die.\n";
+	cout << "Hello. My name is " << yourName << ". You killed my " <<  relative << ". Prepare to die.\n";
 
 
 	return 0; //Tells us the code works
