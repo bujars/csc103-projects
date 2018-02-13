3b60a5f0ebdee71f7b8550b98eec76c1161f66e1
Bujar Sefa <bsefa000@citymail.ccny.cuny.edu>
2018-02-12 22:09:43 -0500

    Uploaded 2nd project. Possible changes to 1st hw messaging

--- p2/primetest.cpp
+++ p2/primetest.cpp
@@ -14,8 +14,23 @@
 using std::cin;
 using std::cout;
 using std::endl;
+#include <cmath> //Incase you figure out the efficency improvement. -- To check how many checks are required and making sure you stop long before n - 2.
+
+// HINT: sqrt function might be useful.
 
 int main()
 {
+ /* NOTE : From what I quickly gathered from the quick skim I did on of the readme file, we are trying to generate all the vlaues of prime, as a value of 1. If the number that is unputted isn't prime, then outout a 0.
+To chcek if a number is prime, we should be able to see if any of the number is odd, that means that its not divisible by 2, unless its the number two itself, and making sure it is not divided by the odd vlues, such as (3, 5, 7, ...., n-2)
+ -- from website -- Keep it simple - just do trial division by odd integers until you either find a divisor, or you run out of candidates.
+ */
+
+
+ /* Thus might help for when to stop the loop
+ unsigned long n;
+ while(cin >> n){
+   //do stuff
+ }
+ */
 	return 0;
 }
