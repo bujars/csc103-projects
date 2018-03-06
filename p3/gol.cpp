d10717564ae442b429e6705f70cec2b33865c4ba
Bujar Sefa <bsefa000@citymail.ccny.cuny.edu>
2018-03-06 01:22:13 -0500

    Some changes/additions on project

--- p3/gol.cpp
+++ p3/gol.cpp
@@ -7,10 +7,11 @@
  * NOT mean it is okay to COPY THAT SOURCE.  What you submit here **MUST BE
  * YOUR OWN WORK**.
  * References:
- *
+ * TA: Ben Wu
+ * stackoverflow.com/questions/19672301/checking-out-of-bounds-in-java  //Helped me realized that I need to make sure my indexes are within 0, the start of the grid, or the vector, column/row length
  *
  * Finally, please indicate approximately how many hours you spent on this:
- * #hours: 
+ * #hours: 8
  */
 
 #include <cstdio>
@@ -63,8 +64,15 @@ int main(int argc, char *argv[]) {
 	// process options:
 	char c;
 	int opt_index = 0;
-	
-	//Parts in Main that you must keep, but don't worry about it. Just know what it accomplishes  
+
+	/* NOTE NOTE NOTE: What this is doing is taking in the filename
+	and everything. Wher eyou usually do cin>>, this code is doing
+	it as provided by the sketlon. This is not something you need to
+	worry about. Updated: March 5th, 2018 -- Told by Ben on March 2
+	nd. */
+
+
+	//Parts in Main that you must keep, but don't worry about it. Just know what it accomplishes
 	while ((c = getopt_long(argc, argv, "hs:w:f:", long_opts, &opt_index)) != -1) {
 		switch (c) {
 			case 'h': //If someone gives -h or --help, it just prints a help screen
@@ -87,7 +95,7 @@ int main(int argc, char *argv[]) {
 	}
 
 	//cin >> file
-	//stdin >> wfile; 
+	//stdin >> wfile;
 
 	/* NOTE: at this point wfilename initfilename and max_gen
 	 * are all set according to the command line. */
@@ -98,10 +106,10 @@ int main(int argc, char *argv[]) {
 }
 
 //Counting neighbors of cell i,j on grid g:
-//not vectors are v[i][j] i = rows j = columns 
+//note vectors are v[i][j] i = rows j = columns ..... They produce a grid...
 size_t nbrCount(size_t i, size_t j, const vector<vector<bool> >& world){
 	for(size_t a = 0, a < world.size(); a++){
-		
+
 	}
 
 }
@@ -109,7 +117,7 @@ size_t nbrCount(size_t i, size_t j, const vector<vector<bool> >& world){
 /* NOTE NOTE NOTE: This is where I bleive the initFromFile(initfilename) function is called. I believe this program is a little similar to the gravity program.... wher eyou input the text name, give it to the program, the program generates the output and then the it is played/presented on the screen. */
 int initFromFile(const string& fname){ /* read initial state into vectors. */
 	//This should be global?... //vector<vector<bool> > world;
-	//NOTE NOTE NOTE its wfname because it was already set...
+	//NOTE NOTE NOTE its wfname because it was already set... as a global variable
 	FILE* f = fopen(wfname.c_str(), "rb"); //Make sure you cin the file name...
 	world.push_back(vector<bool>()); //add a new row....why is this outside the loop? how do we add more?????
 	size_t rows = 0; //Curent row getting filled
@@ -127,7 +135,7 @@ int initFromFile(const string& fname){ /* read initial state into vectors. */
 			world[rows].push_back(true); //set it to true, or in this case alive
 		}
 	}
-	fclose(f); 
+	fclose(f);
 
 	return 0;
 }
@@ -136,7 +144,7 @@ void update() //transform the old version of the world into the new one
 {
 	//AGAIN notices the wfname as this should be the file name variable that was already set at the start.....
 	FILE* f = fopen(wfname.c_str(), "wb");
-	rewind(f); //this is so that we start from the beginning of each file. 
+	rewind(f); //this is so that we start from the beginning of each file.
 	char c = ".";
 	fwrite(&c, 1, 1, f);
 	fclose();
@@ -147,7 +155,7 @@ void mainLoop() {
 	/* TODO: write this */
 	/* update, write, sleep */
 	//Update means get the new positions of the .0
-	//write means reopen the file and rewind all the way up and the transfer the code into .0 
+	//write means reopen the file and rewind all the way up and the transfer the code into .0
 	//sleep means take a pause
 	//sleep(1); //so we take a break
 	//max_gen must be inputted im assuming??
@@ -157,7 +165,11 @@ void mainLoop() {
 		/* go through generations as fast as you can until
 		 * max_gen is reached... */
 	}
-	
+
+
+	///Additioal Notes  If a future generation is given, we do not pause for time, we want it to skip ahead, otherwise keep pausing as we go.
+
+
 
 
 }
