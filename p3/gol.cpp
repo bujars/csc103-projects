df3bad8d56235708d7f69740309a4bd13d573aec
Bujar Sefa <bsefa000@citymail.ccny.cuny.edu>
2018-03-08 13:19:23 -0500

    Made changes doesnt work

--- p3/gol.cpp
+++ p3/gol.cpp
@@ -47,6 +47,10 @@ string wfilename =  "/tmp/gol-world-current"; /* write state here */
 FILE* fworld = 0; /* handle to file wfilename. */
 string initfilename = "/tmp/gol-world-current"; /* read initial state from here. */
 
+vector<vector<bool> > world;
+
+
+
 /* NOTE: you don't have to write these functions -- this is just how
  * I chose to organize my code. */
 
@@ -60,7 +64,7 @@ void dumpState(FILE* f); //write the state to a file
 char text[3] = ".O";
 
 
-vector<vector<bool> > world;
+//vector<vector<bool> > world;
 
 
 int main(int argc, char *argv[]) {
@@ -70,7 +74,7 @@ int main(int argc, char *argv[]) {
 		{"world",   required_argument, 0, 'w'},
 		{"fast-fw", required_argument, 0, 'f'},
 		{"help",    no_argument,       0, 'h'},
-		{0,0,0,0}
+		{-1,0,0,0}
 	};
 	// process options:
 	char c;
@@ -111,7 +115,7 @@ int main(int argc, char *argv[]) {
 	/* NOTE: at this point wfilename initfilename and max_gen
 	 * are all set according to the command line. */
 	/* If you wrote the initFromFile function, call it here: */
-	// initFromFile(initfilename);
+	//int fille = initFromFile(initfilename);
 	mainLoop();
 	return 0;
 }
@@ -119,22 +123,28 @@ int main(int argc, char *argv[]) {
 //Counting neighbors of cell i,j on grid g:
 //note vectors are v[i][j] i = rows j = columns ..... They produce a grid...
 size_t nbrCount(size_t i, size_t j, const vector<vector<bool> >& world){
-	for(size_t a = 0, a < world.size(); a++){
+	/*for(size_t a = 0, a < world.size(); a++){
 
-	}
-
+	}*/
+	return 0; 
 }
 
-/* NOTE NOTE NOTE: This is where I bleive the initFromFile(initfilename) function is called. I believe this program is a little similar to the gravity program.... wher eyou input the text name, give it to the program, the program generates the output and then the it is played/presented on the screen. */
+/* NOTE NOTE NOTE: This is where I believe the initFromFile(initfilename) function is called. 
+ * I believe this program is a little similar to the gravity program.... 
+ * where you input the text name, give it to the program, 
+ * the program generates the output 
+ * and then the it is played/presented on the screen. */
 int initFromFile(const string& fname){ /* read initial state into vectors. */
 	//This should be global?... //vector<vector<bool> > world;
 	//NOTE NOTE NOTE its wfname because it was already set... as a global variable
-	FILE* f = fopen(wfname.c_str(), "rb"); //Make sure you cin the file name...
+#if 0 	
+	FILE* f = fopen(fname.c_str(), "rb"); //Make sure you cin the file name...
 	world.push_back(vector<bool>()); //add a new row....why is this outside the loop? how do we add more?????
 	size_t rows = 0; //Curent row getting filled
+	char c;
 	while(fread(&c, 1, 1, f))
 	{
-		if(cin == "\n"){
+		if(c == '\n'){
 			rows++;
 			world.push_back(vector<bool>());
 		/*NOTE NOTE NOTE this is adding new rows when a new line is deteced, since we are going row by row. */
@@ -147,6 +157,46 @@ int initFromFile(const string& fname){ /* read initial state into vectors. */
 		}
 	}
 	fclose(f);
+#endif
+
+
+
+	//Note this was just copied from Skeiths Stuff.... You stuff was just missing the char c and you had c == "\n" not '\n'
+#if 0
+	vector<vector<bool> > world;
+	FILE* f = fopen(wfilename.c_str(),"rb"); /* note conversion to char* */
+	world.push_back(vector<bool>()); /* add a new row */
+	size_t rows = 0; /* current row we are filling */
+	char c;
+	while (fread(&c,1,1,f)) {
+		if (c == '\n'){
+		/* found newline; add a new row */
+			 rows++;
+			 world.push_back(vector<bool>());
+		} else if (c == '.') {
+			world[rows].push_back(false); /* dead x_x */
+		} else {
+			world[rows].push_back(true); /* alive 8D */
+		 }
+	}
+	fclose(f);
+#endif
+	/* Open the file (for reading), storing the handle in f: */
+	FILE* f = fopen("/path/to/myfile","rb");
+	/* make sure file was opened properly: */
+	if (!f) {
+		    // deal with error by printing a message, or maybe quitting
+			//     // the entire program with a non-zero exit code, like this:
+		 exit(1);
+	}
+			//         /* Read a single character from the file: */
+	char c; // storage for the byte we'll read
+	fread(&c,1,1,f); // read one byte, place it in c
+	/* when you're done with the file, be sure to close it like this: */
+	 fclose(f);
+
+
+
 
 	return 0;
 }
@@ -154,34 +204,40 @@ int initFromFile(const string& fname){ /* read initial state into vectors. */
 void update() //transform the old version of the world into the new one
 {
 	//AGAIN notices the wfname as this should be the file name variable that was already set at the start.....
-	FILE* f = fopen(wfname.c_str(), "wb");
+	/*FILE* f = fopen(fname.c_str(), "wb");
 	rewind(f); //this is so that we start from the beginning of each file.
 	char c = ".";
 	fwrite(&c, 1, 1, f);
-	fclose();
+	fclose(f);*/
 }
 
 
 void mainLoop() {
 	/* TODO: write this */
 	/* update, write, sleep */
+	
+	
 	//Update means get the new positions of the .0
 	//write means reopen the file and rewind all the way up and the transfer the code into .0
 	//sleep means take a pause
 	//sleep(1); //so we take a break
 	//max_gen must be inputted im assuming??
+	
+	
 	if (max_gen == 0) {
 		/* make one generation update per second */
-	} else {
+	} 
+	else {
 		/* go through generations as fast as you can until
 		 * max_gen is reached... */
 	}
 
 
-	///Additioal Notes  If a future generation is given, we do not pause for time, we want it to skip ahead, otherwise keep pausing as we go.
-
-
-
+	///Additioal Notes:  
+	//If a future generation is given, 
+	//we do not pause for time, 
+	//we want it to skip ahead, 
+	//otherwise keep pausing as we go.
 
 }
 
